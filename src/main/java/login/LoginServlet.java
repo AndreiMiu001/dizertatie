acba89bb47cbec18a21bbb737b21ad4b79bac773
Andrei <andrei.miu@nobugconsulting.ro>
2021-01-01 13:49:31 +0200

    Adding BCrypt support for passwords.

--- src/main/java/login/LoginServlet.java
+++ src/main/java/login/LoginServlet.java
@@ -26,55 +26,6 @@ import javax.servlet.http.HttpSession;
  */
 public class LoginServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -83,6 +34,10 @@ public class LoginServlet extends HttpServlet {
             HttpSession session = request.getSession();
             String username = request.getParameter("username");
             String password = request.getParameter("password");
+            UpdatableBCrypt bCrypt = new UpdatableBCrypt(11);
+            String hashedPass = bCrypt.hash(password);
+            boolean unhashedPass = bCrypt.verifyHash(password, hashedPass);
+            boolean x;
             DAO dao = new DAO();
             dao.connect();
             dao.disconnect();
---
+++ src/main/java/login/UpdatableBCrypt.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+import org.mindrot.jbcrypt.*;
+
+public class UpdatableBCrypt {
+    private final int logRounds;
+
+    public UpdatableBCrypt(int logRounds) {
+        this.logRounds = logRounds;
+    }
+
+    public String hash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt(logRounds));
+    }
+
+    public boolean verifyHash(String password, String hash) {
+        return BCrypt.checkpw(password, hash);
+    }
+}

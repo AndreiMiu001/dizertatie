35c734386d9c4466f570ff9601cc7b6d50fae7ef
AndreiMiu001 <miuandrei001@gmail.com>
2021-08-10 19:35:51 +0300

    Add login api and login logic for android app

--- AndroidApp/evotAndroid/app/src/main/java/com/andrei/evot/LoginBW.java
+++ AndroidApp/evotAndroid/app/src/main/java/com/andrei/evot/LoginBW.java
@@ -2,114 +2,135 @@ package com.andrei.evot;
 
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 
-import androidx.appcompat.app.AlertDialog;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class LoginBW extends AsyncTask<String, Void, String> implements AdapterView.OnItemSelectedListener, MyCertificateManager {
 
-    private String cmd = "loginBtn";
-    private String tempResult = "";
-    private int voteFlag = -1;
     private Context context;
+    private boolean status;
 
-    public LoginBW(String tempResult, Context context) {
-        this.tempResult = tempResult;
+    public LoginBW(Context context) {
         this.context = context;
     }
 
     @Override
     protected String doInBackground(String[] strings) {
+        String URL = "http://10.0.2.2:8080/evot/webapi/evot/login";
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        JSONObject postData = new JSONObject();
         try {
-            URL url = new URL(reg_url);
-            if (cmd.equals("loginBtn")) {
-                String post_data = URLEncoder.encode("cmd", "UTF-8") + "=" + URLEncoder.encode(cmd, "UTF-8") + "&" +
-                        URLEncoder.encode("cnp", "UTF-8") + "=" + URLEncoder.encode(User.getCnp(), "UTF-8") + "&" +
-                        URLEncoder.encode("seriesPers", "UTF-8") + "=" + URLEncoder.encode(User.getSeriesPers(), "UTF-8") + "&" +
-                        URLEncoder.encode("numberPers", "UTF-8") + "=" + URLEncoder.encode(User.getNumberPers(), "UTF-8") + "&" +
-                        URLEncoder.encode("idElections", "UTF-8") + "=" + URLEncoder.encode(User.getIdElection_(), "UTF-8");
-                queryServer(post_data, url);
-
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            postData.put("name", User.mCnp);
+            postData.put("password", User.mPassword);
+        } catch (JSONException e) {
             e.printStackTrace();
         }
-        return null;
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                URL,
+                postData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("rest resp", response.toString());
+                        try {
+                            if (response.getBoolean("state")) {
+                                Log.e("rest resp2", "true");
+                                status = true;
+                            } else {
+                                Log.e("rest resp2", "false");
+                                status = false;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
-    }
-
-    private void queryServer(String post_data, URL url) {
-        try {
-            trustAllCertificates();
-            HttpsURLConnection httpURLConnection = (HttpsURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.setDoOutput(true);
-            httpURLConnection.setDoInput(true);
-            OutputStream outputStream = httpURLConnection.getOutputStream();
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
-            bufferedWriter.write(post_data);
-            bufferedWriter.flush();
-            bufferedWriter.close();
-            outputStream.close();
-            InputStream inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.ISO_8859_1));
-            if (cmd.equals("loginBtn")) {
-                String line = "";
-                while ((line = bufferedReader.readLine()) != null) {
-                    tempResult += line;
-                    if (tempResult.equals("PCSC")) {
-                        voteFlag = 1;
-                        tempResult = "";
-                    } else if (tempResult.equals("ERR:EAE")) {
-                        voteFlag = 0;
-                        tempResult = "";
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("rest err", error.toString());
                     }
                 }
-            }
-            bufferedReader.close();
-            inputStream.close();
-            httpURLConnection.disconnect();
-        } catch (
-                MalformedURLException e) {
-            e.printStackTrace();
-        } catch (
-                IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        );
+        requestQueue.add(objectRequest);
+        return null;
     }
 
+    /*  private void queryServer(String post_data, URL url) {
+          try {
+              trustAllCertificates();
+              HttpsURLConnection httpURLConnection = (HttpsURLConnection) url.openConnection();
+              httpURLConnection.setRequestMethod("POST");
+              httpURLConnection.setDoOutput(true);
+              httpURLConnection.setDoInput(true);
+              OutputStream outputStream = httpURLConnection.getOutputStream();
+              BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
+              bufferedWriter.write(post_data);
+              bufferedWriter.flush();
+              bufferedWriter.close();
+              outputStream.close();
+              InputStream inputStream = httpURLConnection.getInputStream();
+              BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.ISO_8859_1));
+              if (cmd.equals("loginBtn")) {
+                  String line = "";
+                  while ((line = bufferedReader.readLine()) != null) {
+                      tempResult += line;
+                      if (tempResult.equals("PCSC")) {
+                          voteFlag = 1;
+                          tempResult = "";
+                      } else if (tempResult.equals("ERR:EAE")) {
+                          voteFlag = 0;
+                          tempResult = "";
+                      }
+                  }
+              }
+              bufferedReader.close();
+              inputStream.close();
+              httpURLConnection.disconnect();
+          } catch (
+                  MalformedURLException e) {
+              e.printStackTrace();
+          } catch (
+                  IOException e) {
+              e.printStackTrace();
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+      }
+  */
     @Override
     protected void onPostExecute(String s) {
-        if (tempResult.equals("MBSC") && voteFlag == 1) {
+        if (status == true) {
             try {
-
                 Intent showDetailActivity = new Intent(context, VotingScreen.class);
                 context.startActivity(showDetailActivity);
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+        /*
+        if (tempResult.equals("MBSC") && voteFlag == 1) {
+
         } else if (tempResult.equals("ERR:WOIC") && voteFlag == 1) {
             AlertDialog.Builder builder = new AlertDialog.Builder(context);
             builder.setMessage("Date personale eronate").setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -136,7 +157,7 @@ public class LoginBW extends AsyncTask<String, Void, String> implements AdapterV
             });
             AlertDialog alert = builder.create();
             alert.show();
-        }
+        }*/
     }
 
     @Override
